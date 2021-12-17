74ee101625b1a2a35064cb025d8490dc0244c1b3
Nicolas Ballaman <nicolasballaman@gmail.com>
2021-12-17 13:10:27 -0300

    mint function and tests

--- contracts/Color.sol
+++ contracts/Color.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: ISC
 pragma solidity >=0.8.0;
 
-import "./ERC721.sol";
+import "./ERC721Enumerable.sol";
 
-contract Color is ERC721{
+contract Color is ERC721Enumerable{
     string constant _name = "Color NFT";
     string constant _symbol = "CLR";
-    constructor() ERC721(_name, _symbol) {
+    string[] public colors;
+    mapping(string => bool) _colorExists;
 
+    constructor() ERC721(_name, _symbol) {}
+
+    function mint(string memory _color) public {
+        require(!_colorExists[_color]);
+        colors.push(_color);
+        uint _id = colors.length - 1;
+        _mint(msg.sender, _id);
+        _colorExists[_color] = true;
     }
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -16,7 +16,7 @@ contract('Color', (accounts)=>{
         contract = await colorContract.deployed();
     });
     describe('deployment', async () => {
-
+        
         it('deploys syccessfully', async () => {
             const contractAddress = contract.address;
             assert.notEqual(contractAddress, '');
@@ -29,11 +29,42 @@ contract('Color', (accounts)=>{
             const contractName = await contract.name();
             assert.equal(contractName, _contractName);
         });
-
+        
         it('has a symbol', async () => {
             const contractSymbol = await contract.symbol();
             assert.equal(contractSymbol, _contractSymbol);
         });
-
+        
+    });
+    
+    describe('minting', async () => {
+        it('creates a new token', async () => {
+            const result = await contract.mint('#000000');
+            const totalSupply = await contract.totalSupply();
+            //success
+            assert.equal(totalSupply, 1);
+            const event = result.logs[0].args;
+            assert.equal(event.tokenId.toNumber(), 0, 'tokenId is correct');
+            assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct');
+            assert.equal(event.to, accounts[0], 'to is correct');
+            //failure: can't mint same color twice
+            await contract.mint('#000000').should.be.rejected;
+            
+        });
+    });
+    describe('indexing', async () => {
+        it('lists colors', async () => {
+            let expected = ['#000000', '#000001', '#000002', '#000003']
+            await contract.mint('#000001');
+            await contract.mint('#000002');
+            await contract.mint('#000003');
+            const totalSupply = await contract.totalSupply();
+            let result= [];
+            for (let i = 0; i < totalSupply; i++) {
+                const color = await contract.colors(i);
+                result.push(color);   
+            }
+            assert.equal(result.join(','), expected.join(','))
+        });
     });
 });
